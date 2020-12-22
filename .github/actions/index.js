9e3d30e3d67511ef43c247e5d0a9b7c0b237e503
Juyeong Jeong <dosel1005@dgist.ac.kr>
2020-12-22 16:25:13 +0900

    11111

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -1,12 +1,11 @@
+import FS from "fs";
+
 console.log("나는 낭만 고양이~");
 console.log("슬픈 도시를 비춰~");
 console.log("춤추는 작은 별빛~");
 
-console.log("ㅡㅡㅡ구구단 출력ㅡㅡㅡ");
-for(let i = 2; i <= 9; i++){
-  console.log(`ㅡㅡㅡ${i} 단ㅡㅡㅡ`);
-  for(let j = 1; j <= 9; j++){
-    console.log(`${i} x ${j} = ${i * j}`);
-  }
-}
+console.log(
+  JSON.stringify(JSON.parse(FS.readFileSync("./endpoints.json").toString()), null, 2)
+);
+
 process.exit(0);
